3cd930c5d74e03da13879bdc9304f4f35860b954
weed97 <weed9935@gmail.com>
2024-07-12 10:14:55 +0900

    first commit

---
+++ chap00-intro-lecture-source/src/main/java/com/ohgiraffers/section01/problem/Category.java
@@ -0,0 +1,15 @@
+package com.ohgiraffers.section01.problem;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Category {
+
+    private int categoryCode;
+    private String categoryName;
+
+}
---
+++ chap00-intro-lecture-source/src/main/java/com/ohgiraffers/section01/problem/MenuAndCategory.java
@@ -0,0 +1,17 @@
+package com.ohgiraffers.section01.problem;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class MenuAndCategory {
+
+    private int menuCode;
+    private String menuName;
+    private int menuPrice;
+    private Category categoryCode;
+    private String orderableStatus;
+}
